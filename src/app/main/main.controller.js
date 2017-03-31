d98856f32a9ebb0331ddb0a7830d29cb1c550680
YareArista <yarearista@gmail.com>
2017-03-30 20:44:30 -0600

    Vista responsive y funcionalidad de los links internos, faltan CTA de redes sociales y descarga de la app

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,44 +4,24 @@
   angular
     .module('landingRu')
     .controller('MainController', MainController)
-    .directive('startslider',function($timeout) {
-    
-});
+    .run(['$anchorScroll', function($anchorScroll) {
+      $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
+  }]);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1471300053273;
-    vm.showToastr = showToastr;
-    vm.pictures = [];
-    for (var i=1; i<=16;i++){
-      vm.pictures.push({src: '/assets/images/' + i +'.png'});
-    }
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+  function MainController($anchorScroll, $location, $scope) {
+    $scope.gotoAnchor = function(x) {
+      var newHash = 'anchor' + x;
+      if ($location.hash() !== newHash) {
+        // set the $location.hash to `newHash` and
+        // $anchorScroll will automatically scroll to it
+        $location.hash('anchor' + x);
+      } else {
+        // call $anchorScroll() explicitly,
+        // since $location.hash hasn't changed
+        $anchorScroll();
+      }
+    };
   }
 })();
 
