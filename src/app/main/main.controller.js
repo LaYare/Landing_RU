cf76120cde99f03113cc4352be19d904660878ba
Luis Rayon <luarv90@gmail.com>
2017-05-16 18:15:35 -0500

    Add functions for email forms and connect to email api

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,7 +9,7 @@
   }]);
 
   /** @ngInject */
-  function MainController($anchorScroll, $location, $scope) {
+  function MainController($anchorScroll, $location, $scope, $http, $window) {
     $scope.gotoAnchor = function(x) {
       var newHash = 'anchor' + x;
       if ($location.hash() !== newHash) {
@@ -22,6 +22,22 @@
         $anchorScroll();
       }
     };
+    $scope.user = {};
+
+$scope.addPost = function () {
+
+      $scope.user.page="retouniversidad.com";
+   $http.post("http://emailapi.mellow.online/api/general",$scope.user)
+       .success(function (data, status, headers, config) {
+           $window.location.reload();
+           console.log(data);
+       })
+       .error(function (data, status, header, config) {
+            console.log(data);
+       });
+
+};
+
   }
 })();
 
